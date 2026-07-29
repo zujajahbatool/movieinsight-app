6bb47b80dae679c81371c30daf386daf77ad8352
zujajahbatool <batoolzujajah@gmail.com>
2026-07-29 23:29:47 +0500

    updated the App.jsx code for new components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import Movies from './components/home/Movies/Movies';
 import GoldenGlobeBanner from './components/home/GoldenGlobeBanner/GoldenGlobeBanner';
 import Series from './components/home/Series/Series';
 import Pricing from './components/home/Pricing/Pricing';
+import Collections from './components/home/Collections/Collections';
+import ContinueWatching from './components/home/ContinueWatching/ContinueWatching';
+import Characters from './components/home/Characters/Characters';
+import KidsBanner from './components/home/KidsBanner/KidsBanner';
 import { useThemeStore } from './store/useThemeStore';
 import './App.css';
 
@@ -27,6 +31,10 @@ function App() {
         <GoldenGlobeBanner />
         <Series />
         <Pricing />
+        <Collections />
+        <ContinueWatching />
+        <Characters />
+        <KidsBanner />
       </main>
     </div>
   );
