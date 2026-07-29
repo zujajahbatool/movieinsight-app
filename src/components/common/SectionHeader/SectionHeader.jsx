8c96d2bd9ca2138c7a406f601a9dd1b3d1d0c68c
zujajahbatool <batoolzujajah@gmail.com>
2026-07-29 23:17:51 +0500

    added functionality to the section header to handle multiple button types

--- src/components/common/SectionHeader/SectionHeader.jsx
+++ src/components/common/SectionHeader/SectionHeader.jsx
@@ -2,18 +2,22 @@ import { ArrowRight } from 'lucide-react';
 import Button from '../Button/Button';
 import styles from './SectionHeader.module.css';
 
-function SectionHeader({ title, onSeeMore }) {
+function SectionHeader({ title, onSeeMore, rightContent }) {
   return (
     <div className={styles['section-header']}>
       <h2 className={styles['section-header__title']}>{title}</h2>
-      {onSeeMore && (
-        <Button
-          variant="link"
-          onClick={onSeeMore}
-          rightIcon={<ArrowRight size={18} strokeWidth={2} />}
-        >
-          See More
-        </Button>
+      {rightContent ? (
+        rightContent
+      ) : (
+        onSeeMore && (
+          <Button
+            variant="link"
+            onClick={onSeeMore}
+            rightIcon={<ArrowRight size={18} strokeWidth={2} />}
+          >
+            See More
+          </Button>
+        )
       )}
     </div>
   );
