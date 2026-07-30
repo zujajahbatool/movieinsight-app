819315be432451dad95b25067491fbad0ba7c73e
zujajahbatool <batoolzujajah@gmail.com>
2026-07-30 22:49:04 +0500

    added the popular characters section of the kids page

---
+++ src/components/kids/PopularCharacters/PopularCharacters.jsx
@@ -0,0 +1,75 @@
+import ScrollRow from '../../common/ScrollRow/ScrollRow';
+import MovieCard from '../../common/MovieCard/MovieCard';
+import styles from './PopularCharacters.module.css';
+
+// Predefined character-centric poster paths from TMDB
+const POPULAR_CHARACTERS = [
+  {
+    id: 'char-tomjerry',
+    name: 'Tom & Jerry',
+    posterUrl: 'https://www.themoviedb.org/t/p/w600_and_h900_face/pApniCXhegDnhxQda7RcpG8gFP0.jpg', 
+    rating: 8.5,
+    genre: 'Tom & Jerry',
+
+  },
+  {
+    id: 'char-anger',
+    name: 'Anger',
+    posterUrl: 'https://image.tmdb.org/t/p/w342/2H1TmgdfNtsKlU9jKdeNyYL5y8T.jpg', // Inside Out
+    rating: 8.9,
+    genre: 'Inside Out',
+  },
+  {
+    id: 'char-elsa',
+    name: 'Elsa',
+    posterUrl: 'https://image.tmdb.org/t/p/w342/mINJaa34MtknCYl5AjtNJzWj8cD.jpg', // Frozen II
+    rating: 9.0,
+    genre: 'Frozen II',
+  },
+  {
+    id: 'char-moana',
+    name: 'Moana',
+    posterUrl: 'https://image.tmdb.org/t/p/w342/zKVgiv5qHCvCLT4A2ymJi5QeXDH.jpg', // Moana
+    rating: 8.8,
+    genre: 'Moana',
+  },
+  {
+    id: 'char-vanellope',
+    name: 'Vanellope',
+    posterUrl: 'https://image.tmdb.org/t/p/w342/ihfvxPh5C4rV2WSKWs7k7rzxzzO.jpg', // Wreck-It Ralph
+    rating: 8.6,
+    genre: 'Wreck-It Ralph',
+  },
+  {
+    id: 'char-boss-baby',
+    name: 'Boss Baby',
+    posterUrl: 'https://image.tmdb.org/t/p/w342/9MsQJKe4cUAGxc7R2NGaFQLqOPc.jpg', // The Boss Baby
+    rating: 8.2,
+    genre: 'The Boss Baby',
+  },
+];
+
+function PopularCharacters() {
+  const handleSeeMore = () => {
+    console.log('Navigate to Popular Characters See More');
+  };
+
+  return (
+    <div className={styles.characters}>
+      <ScrollRow title="Popular Character" onSeeMore={handleSeeMore}>
+        {POPULAR_CHARACTERS.map((char) => (
+          <MovieCard
+            key={char.id}
+            title={char.name}
+            posterUrl={char.posterUrl}
+            rating={char.rating}
+            genre={char.genre}
+            variant="simple"
+          />
+        ))}
+      </ScrollRow>
+    </div>
+  );
+}
+
+export default PopularCharacters;
