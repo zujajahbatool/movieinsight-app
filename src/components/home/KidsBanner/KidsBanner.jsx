f27656d2253f33da777b5a87cc24414022994754
zujajahbatool <batoolzujajah@gmail.com>
2026-07-30 22:39:57 +0500

    added link functionality to kids banner and made it to direct to the kids page when clicked

--- src/components/home/KidsBanner/KidsBanner.jsx
+++ src/components/home/KidsBanner/KidsBanner.jsx
@@ -1,10 +1,16 @@
+import { useNavigationStore } from '../../../store/useNavigationStore';
 import kidsImg from '../../../assets/kids.png';
 import styles from './KidsBanner.module.css';
 
 function KidsBanner() {
+  const setPage = useNavigationStore((state) => state.setPage);
+
   return (
     <section className={styles['kids-banner']}>
-      <div className={styles['kids-banner__container']}>
+      <div
+        className={styles['kids-banner__container']}
+        onClick={() => setPage('kids')}
+      >
         <img
           src={kidsImg}
           alt="Family Friendly Streaming - Children's Section"
