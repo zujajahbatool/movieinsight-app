519f503a61bd395654e8763f0f8ea0f263ae90fc
zujajahbatool <batoolzujajah@gmail.com>
2026-07-31 15:49:05 +0500

    built the series page

---
+++ src/pages/SeriesPage.jsx
@@ -0,0 +1,7 @@
+import MediaPageLayout from '../components/layout/MediaPageLayout/MediaPageLayout';
+
+function SeriesPage() {
+  return <MediaPageLayout title="Series" mediaType="tv" />;
+}
+
+export default SeriesPage;
