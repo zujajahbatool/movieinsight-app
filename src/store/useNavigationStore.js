b8000e8b3fdc9050aff5a485cda29f015e3f03ed
zujajahbatool <batoolzujajah@gmail.com>
2026-08-01 17:25:06 +0500

    added the navigation storage logic

--- src/store/useNavigationStore.js
+++ src/store/useNavigationStore.js
@@ -1,7 +1,31 @@
 import { create } from 'zustand';
 
 export const useNavigationStore = create((set) => ({
-  page: 'home', // 'home' | 'kids' | 'movies' | 'series' | 'advance-search'
-  setPage: (page) => set({ page }),
+  page: 'home', // 'home' | 'kids' | 'movies' | 'series' | 'advance-search' | 'collection-list' | 'single-collection' | 'watch-now' | 'login'
+  previousPage: 'home',
+  selectedCollection: null,
+  watchNowId: null,
+  watchNowType: 'movie', // 'movie' | 'tv'
+  isKidsWatch: false,
+  setPage: (page) => set((state) => ({ previousPage: state.page, page })),
+  setSelectedCollection: (selectedCollection) => set({ selectedCollection }),
+  setWatchNow: (id, mediaType = 'movie', isKids = false) =>
+    set((state) => ({
+      previousPage: state.page,
+      page: 'watch-now',
+      watchNowId: id,
+      watchNowType: mediaType,
+      isKidsWatch: isKids,
+    })),
+  playVideo: (id, mediaType = 'movie', isKids = false) =>
+    set((state) => ({
+      previousPage: state.page,
+      page: 'video-player',
+      watchNowId: id,
+      watchNowType: mediaType,
+      isKidsWatch: isKids,
+    })),
 }));
+
+
 
