4cd22a5cb1556608670da2b1a67b1b7054633b32
zujajahbatool <batoolzujajah@gmail.com>
2026-08-01 17:12:46 +0500

    added the link to the watch-now page

--- src/components/kids/KidsSuggestions/KidsSuggestions.jsx
+++ src/components/kids/KidsSuggestions/KidsSuggestions.jsx
@@ -3,12 +3,14 @@ import ScrollRow from '../../common/ScrollRow/ScrollRow';
 import MovieCard from '../../common/MovieCard/MovieCard';
 import { getPosterUrl } from '../../../api/tmdbClient';
 import { useWatchlistStore } from '../../../store/useWatchlistStore';
+import { useNavigationStore } from '../../../store/useNavigationStore';
 import styles from './KidsSuggestions.module.css';
 
 function KidsSuggestions() {
   const { data: items, loading, error } = useKidsContent('suggestions', 12);
   const watchlist = useWatchlistStore((state) => state.watchlist);
   const toggleMovie = useWatchlistStore((state) => state.toggleMovie);
+  const setWatchNow = useNavigationStore((state) => state.setWatchNow);
 
   const handleSeeMore = () => {
     console.log('Navigate to Kids Suggestions See More');
@@ -33,6 +35,7 @@ function KidsSuggestions() {
               genreIds={item.genre_ids}
               isAdded={watchlist.includes(item.id)}
               onAddToggle={() => toggleMovie(item.id)}
+              onClick={() => setWatchNow(item.id, 'movie', true)}
             />
           ))}
         </ScrollRow>
