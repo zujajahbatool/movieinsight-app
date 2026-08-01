4563d499e2ea40b76991ac0577ead085f61b0cd2
zujajahbatool <batoolzujajah@gmail.com>
2026-08-01 17:30:33 +0500

    added constant movies and series data with ids, title, hero-title, genre, values etc.

---
+++ src/constants/collectionsData.js
@@ -0,0 +1,25 @@
+export const MOVIE_COLLECTIONS = [
+  { id: 'musicals', title: 'Musicals', heroTitle: 'Broadway & Musicals Collection', type: 'genre', value: 10402 },
+  { id: 'marvel', title: 'Marvel', heroTitle: 'Marvel Cinematic Universe', type: 'search', value: 'Marvel' },
+  { id: 'dc', title: 'DC', heroTitle: 'The Dark Knight Collection', type: 'search', value: 'DC Comics' },
+  { id: 'johnwick', title: 'John Wick', heroTitle: 'John Wick Collection', type: 'search', value: 'John Wick' },
+  { id: 'godzilla', title: 'Godzilla', heroTitle: 'Godzilla Collection', type: 'search', value: 'Godzilla' },
+  { id: 'indianajones', title: 'Indiana Jones', heroTitle: 'Indiana Jones Collection', type: 'search', value: 'Indiana Jones' },
+  { id: 'diehard', title: 'Die Hard', heroTitle: 'Die Hard Collection', type: 'search', value: 'Die Hard' },
+  { id: 'starwars', title: 'Star Wars', heroTitle: 'Star Wars Collection', type: 'search', value: 'Star Wars' },
+  { id: 'thenun', title: 'The Nun', heroTitle: 'The Conjuring Universe', type: 'search', value: 'The Nun' },
+  { id: 'insidious', title: 'Insidious', heroTitle: 'Insidious Collection', type: 'search', value: 'Insidious' },
+];
+
+export const SERIES_COLLECTIONS = [
+  { id: 'anime', title: 'Anime', heroTitle: 'Anime Masterpieces', type: 'search', value: 'Anime' },
+  { id: 'marvel_tv', title: 'Marvel', heroTitle: 'Marvel TV Series', type: 'search', value: 'Marvel' },
+  { id: 'dc_tv', title: 'DC', heroTitle: 'DC TV Series', type: 'search', value: 'DC Comics' },
+  { id: 'scifi_tv', title: 'Sci-Fi', heroTitle: 'Star Trek Collection', type: 'search', value: 'Star Trek' },
+  { id: 'crime_tv', title: 'Crime', heroTitle: 'Sherlock & Crime Series', type: 'search', value: 'Sherlock' },
+  { id: 'comedy_tv', title: 'Comedy', heroTitle: 'Sitcoms Collection', type: 'search', value: 'Comedy' },
+  { id: 'supernatural', title: 'Supernatural', heroTitle: 'Supernatural Series', type: 'search', value: 'Supernatural' },
+  { id: 'friends', title: 'Friends', heroTitle: 'Friends Series Collection', type: 'search', value: 'Friends' },
+  { id: 'got', title: 'Game of Thrones', heroTitle: 'Game of Thrones Saga', type: 'search', value: 'Game of Thrones' },
+  { id: 'breakingbad', title: 'Breaking Bad', heroTitle: 'Breaking Bad Collection', type: 'search', value: 'Breaking Bad' },
+];
